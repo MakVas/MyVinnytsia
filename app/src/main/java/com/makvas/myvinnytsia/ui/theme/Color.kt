28aad45845eafd5029557662338182b7e0c41957
makvas <andriymvnaruto@gmail.com>
2025-02-05 14:04:17 +0200

    Changed app theme and logo

--- app/src/main/java/com/makvas/myvinnytsia/ui/theme/Color.kt
+++ app/src/main/java/com/makvas/myvinnytsia/ui/theme/Color.kt
@@ -2,10 +2,9 @@ package com.makvas.myvinnytsia.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val BackgroundGray = Color(0xfff1f0f0)
+val SurfaceWhite = Color(0xffffffff)
+val PrimaryRed = Color(0xffff3939)
+val SecondaryBlue = Color(0xff7AB0DE)
+val TertiaryGreen = Color(0xff28a43c)
+val TextBlack = Color(0xff000000)
